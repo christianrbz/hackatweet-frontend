a55dfb40ed23ff709c7c4c141e6d94d635378e9b
Triple Patte <triplepatte123@gmail.com>
2023-03-23 12:11:59 +0100

    settings Redux installation

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,47 @@
 import '@/styles/globals.css'
 
+import '../styles/globals.css';
+import Head from 'next/head';
+//import Header from '../components/Header';
+
+import { Provider } from 'react-redux';
+
+import users from '../reducers/users';
+
+
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+
+const reducers = combineReducers({users});
+const persistConfig = { key: 'hackatweet', storage };
+
+const store = configureStore({
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+  });
+
+
+
+const persistor = persistStore(store);
+
+
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+      <Head>
+        <title>Morning News</title>
+      </Head>
+     
+      <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  );
 }
